0a73a72620f77a702ff877feb0e588fe6bb20819
Anonyx-Byte <moikp811@gmail.com>
2026-03-10 00:05:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 ﻿// backend/index.js
 const path = require("path");
-const crypto = require("crypto");
+const crypto = require("crypto"); 
 const util = require("util");
 const dotenv = require("dotenv");
 const bcrypt = require("bcrypt");
